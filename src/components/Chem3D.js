933b6117b1e56ea5690eb1e706b555b243e667fc
Camilo U <cauribeva@unal.edu.co>
2021-07-15 10:40:03 -0500

    Chem3D es ahora responsive

--- src/components/Chem3D.js
+++ src/components/Chem3D.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 
 const Chem3D = (cid) => {
 
-  var url = 'https://embed.molview.org/v1/?mode=balls&cid='+cid.cid;
+  const [size, setSize] = useState(window.innerWidth);
+  const breakpoint = 1000;
+  const breakpoint_sm = 650;
 
-  var content ="<iframe style='width: 500px; height: 300px; background-color: Snow;' frameborder='0' src=" + url + "></iframe>";
+  
+  useEffect(()=>{    
+    const handleWindowResize = () => setSize(window.innerWidth)
+    window.addEventListener("resize", handleWindowResize)
+    return () => window.removeEventListener("resize", handleWindowResize)}
+  ,[])
 
+  let url = 'https://embed.molview.org/v1/?mode=balls&cid='+cid.cid;
 
-  return(
-    <div dangerouslySetInnerHTML={{__html: content}}></div>
+  const content= {__html: "<iframe style='width: 550px; height: 450px; background-color: Snow;' frameborder='0' src=" + url + "></iframe>"};
+  const content_medium = {__html: "<iframe style='width: 420px; height: 350px; background-color: Snow;' frameborder='0' src=" + url + "></iframe>"};
+  const content_small = {__html: "<iframe style='width: 270px; height: 250px; background-color: Snow;' frameborder='0' src=" + url + "></iframe>"};
 
-  );
+ return size  > breakpoint ? <div dangerouslySetInnerHTML={content}/> : size > breakpoint_sm ? <div dangerouslySetInnerHTML={content_medium}/>: <div dangerouslySetInnerHTML={content_small}/>;
+
+  
 
 };
 
